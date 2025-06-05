2f354ab2d195e2bd571e063f271f8cc13aec609f
Nicolas-Motta <155629598+Nicolas-Motta@users.noreply.github.com>
2025-06-05 13:37:09 +0000

    Add initial CSS styles and structure for Index component

--- project/src/Index/Index.jsx
+++ project/src/Index/Index.jsx
@@ -4,6 +4,18 @@ import "./Index.css";
 export default function Index() {
     return (
         <main>
+            <header>
+                <img src="" alt="logo" />
+            </header>
+
+            <div id="title">
+                <h1>AnimalIA</h1>
+                <h3>Project ML</h3>
+            </div>
+            
+            <p></p>
+
+            
 
         </main>
     )
